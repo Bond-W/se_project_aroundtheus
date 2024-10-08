ac5966d8bca807ff198ff5cb3e0d33ef73a97354
bond-wyatt <bondwyatt36@gmail.com>
2024-10-07 21:10:57 -0500

    api class added modals started

---
+++ src/components/Api.js
@@ -0,0 +1,153 @@
+export default class Api {
+    constructor({ baseUrl, headers }) {
+        this._baseUrl = baseURL;
+        this._headers + headers;
+    }
+
+    getInitialCards() {
+        return fetch(`${this._baseUrl}/cards`, {
+            method: "GET",
+            headers: this._headers,
+        })
+        .then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(`Error: ${res.status}`);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+    }
+
+    getUserInfo() {
+        return fetch(`${this._baseUrl}/users/me`, {
+            method: "GET",
+            headers: this._headers,
+        })
+        .then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(`Error: ${res.status}`);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+    }
+
+    updateUserInfo(data) {
+        return fetch(`${this._baseUrl}/users/me`, {
+            method: "PATCH",
+            headers: this._headers,
+            body: JSON.stringify({
+                name: data.name,
+                about: data.job,
+            }),
+        })
+        .then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(`Error: ${res.status}`);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+    }
+
+    addCard(data) {
+        return fetch(`${this._baseUrl}/cards`, {
+            method: "POST",
+            headers: this._headers,
+            body: JSON.stringify({
+                name: data.title,
+                link: data.url,
+            }),
+        })
+        .then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(`Error: ${res.status}`);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+    }
+
+    deleteCard(cardId) {
+        return fetch(`${this._baseUrl}/cards/${cardId}`, {
+            method: "DELETE",
+            headers: this._headers,
+        })
+        .then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(`Error: ${res.status}`);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+    }
+
+    likeCard(cardId) {
+        return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
+            method: "PUT",
+            headers: this._headers,
+        })
+        .then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(`Error: ${res.status}`);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+    }
+
+    unlikeCard(cardId) {
+        return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
+            method: "DELETE",
+            headers: this._headers,
+        })
+        .then((res) => {
+            if (res.ok) {
+                return res.json();
+            }
+            return Promise.reject(`Error: ${res.status}`);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+    }
+
+    updateUserAvatar(avatarUrl) {
+        return fetch(`${this._baseUrl}/users/me/avatar`, {
+            method: "PATCH",
+            headers: this._headers,
+            body: JSON.stringify({
+                avatar: avatarUrl,
+            }),
+        })
+        .then((res) => {
+            if(res.ok) {
+                return res.json();
+            }
+            return Promise.reject(`Error: ${res.status}`);
+        })
+        .catch ((err) => {
+            console.error(err);
+        });
+    }
+}
+
+const api = new Api({
+    baseUrl: "https://around-api.en.tripleten-services.com/v1",
+    headers: {
+        authorization: "19ef03f7-30e6-4001-8bf7-8913888ed1f1",
+        "Content-Type": "application/json",
+    },
+});
